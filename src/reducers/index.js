61db3ffde3049fc69ad6cafedfd77d0f61071c98
Ben Cullen <benjaminjcullen1@gmail.com>
2022-07-11 11:38:52 +0100

    added reducer for song list and selected song, added action for selected song

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+	return [
+		{ title: "Enter Sandman", duration: "5:31" },
+		{ title: "Master of Puppets", duration: "8:35" },
+		{ title: "Whiskey in the Jar", duration: "5:04" },
+		{ title: "Fade To Black", duration: "6:57" },
+		{ title: "Fuel", duration: "4:29" },
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === "SONG_SELECTED") {
+		return action.payload;
+	}
+	return selectedSong;
+};
